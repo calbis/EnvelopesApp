1e41ba9f912904e992bdbefad1a15806de19e9ad
calvinb <calvinb@calvinb>
2014-12-03 09:03:07 -0500

    Start of getting vw_envelope_sum view working

--- basic/database/functions/fn_calculate_envelope_stats_length.sql
+++ basic/database/functions/fn_calculate_envelope_stats_length.sql
@@ -1,53 +1,57 @@
-CREATE FUNCTION [dbo].[fnCalculateEnvelopeStatsLength]
+Drop Function If Exists fn_calculate_envelope_stats_length;
+
+Delimiter $$
+
+Create Function fn_calculate_envelope_stats_length
 (
-	@envelopeId int,
-	@userId int
+	envelopeId int
 )
-RETURNS Decimal(19, 2)
-AS
-BEGIN
-	Declare @length int;
-	Select @length = 
-	(
-		Select U.StatsLength
-		From Users U
-		Where U.Id = @userId
-	);
-	Declare @display varchar(1);
-	Select @display = 
-	(
-		Select U.StatsDisplay
-		From Users U
-		Where U.Id = @userId
-	);
+Returns Decimal(19, 2)
+Not Deterministic
+Begin
+    Declare lengt int;
+    Select U.StatsLength
+    Into lengt
+    From Users U
+    Where U.Id = 1;
 
-	Declare @date date;
-	Select @date = Case When @display = 'm' Then DATEADD (MONTH, @length * -1, GETDATE()) Else DATEADD (DAY, @length * -7, GETDATE()) End;
+    Declare display varchar(1);
+    Select U.StatsDisplay
+    Into display
+    From Users U
+    Where U.Id = 1;
 
-	Declare @statsCost decimal(19, 2);
-	Select @statsCost = dbo.fnCalculateEnvelopeStatsCost(@envelopeId, @userId);
+    Declare dat date;
+    Select Case When display = 'm' Then DATEADD (MONTH, lengt * -1, GETDATE()) Else DATEADD (DAY, lengt * -7, GETDATE()) End Into dat;
 
-	Declare @retVal decimal(19, 2);
-	
-	Select @retVal = 	
-	(
-		Select Case When @statsCost = 0 Then
-			0
-		Else
-		(
-			Select Top 1
-			ISNULL
-			(
-				(
-					Select (UE.PendingSum + UE.AmountSum) / @statsCost
-				)
-			, 0.0)
-			From vwUserEnvelopes UE
-			Where UE.EnvelopeId = @envelopeId
-				And UE.UserId = @userId
-		)
-		End
-	);
+--     Declare statsCost decimal(19, 2);
+--     --Select fnCalculateEnvelopeStatsCost(envelopeId) Into statsCost;
+--     Select 100 Into statsCost;
+-- 
+     Declare retVal decimal(19, 2);
+Set retVal = 3.55;
+-- 
+--     Select retVal = 	
+--     (
+--             Select Case When statsCost = 0 Then
+--                     0
+--             Else
+--             (
+--                     Select Top 1
+--                     ISNULL
+--                     (
+--                             (
+--                                     Select (UE.PendingSum + UE.AmountSum) / statsCost
+--                             )
+--                     , 0.0)
+--                     From vwUserEnvelopes UE
+--                     Where UE.EnvelopeId = envelopeId
+--                             And UE.UserId = userId
+--             )
+--             End
+--     );
+-- 
+     RETURN retVal;
+End $$
 
-	RETURN @retVal;
-END
+Delimiter ;
