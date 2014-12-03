9bc61a22d1289905cab0552f3175dd485e0234f7
calvinb <calvinb@calvinb>
2014-12-02 20:57:09 -0500

    fixed file permissions for production environments

---
+++ basic/database/views/vw_envelope_sum.sql
@@ -0,0 +1,28 @@
+Create Or Replace View vw_envelope_sum
+    As Select E.Id EnvelopeId
+        , Case When A.IsCash= 1 Then 0.00 Else Sum(IFNULL(Tran.Amount, 0.0)) End EnvelopeSum
+        , Case When A.IsCash= 1 Then 0.00 Else Sum(IFNULL(Tran.Pending, 0.0)) End EnvelopePending
+    From `account` A
+    Inner Join `envelope` E
+        On A.Id = E.AccountId
+            And A.IsDeleted = 0
+            And E.IsDeleted = 0
+    Inner Join `transaction` Tran
+        On E.Id = Tran.EnvelopeId
+            And Tran.IsDeleted = 0
+    Group By E.Id;
+
+
+Create Or Replace View vw_account_sum As
+    Select A.Id AccountId
+        , Case When A.IsCash= 1 Then 0.00 Else Sum(IFNULL(Tran.Amount, 0.0)) End AccountSum
+        , Case When A.IsCash= 1 Then 0.00 Else Sum(IFNULL(Tran.Pending, 0.0)) End AccountPending
+    From `account` A
+    Inner Join `envelope` E
+        On A.Id = E.AccountId
+            And A.IsDeleted = 0
+            And E.IsDeleted = 0
+    Inner Join `transaction` Tran
+        On E.Id = Tran.EnvelopeId
+            And Tran.IsDeleted = 0
+    Group By A.Id;
--- basic/views/envelope/index.php
+++ basic/views/envelope/index.php
@@ -15,15 +15,88 @@ $this->params['breadcrumbs'][] = $this->title;
 
 
 <?php $this->beginBlock('main-content'); ?>
-
-<h2><?= Html::encode($this->title) ?></h2>
-<div class="editControlSwitch" style="float: right;">
+<div>
+    <table>
+        <thead></thead>
+        <tbody>
+            <tr>
+                <td>Account Grand Total</td>
+                <td>100.00</td>
+            </tr>
+            <tr>
+                <td>Account Pending</td>
+                <td>75.00</td>
+            </tr>
+            <tr style="font-weight: bold;">
+                <td>Current Account Total hdnAccountId</td>
+                <td>25.00</td>
+            </tr>
+            <tr>
+                <td>From WebSite</td>
+                <td>txtExternalTotal</td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" value="Calculate" /></td>
+                <td>
+                    Conditional Add Transaction button
+                    <?php
+//                    
+//                            @if (ViewBag.Account.ExternalTotal - ViewBag.Account.AmountSum != 0)
+//                            {
+//                                @(ViewBag.Account.ExternalTotal - ViewBag.Account.AmountSum)
+//                                <a class="showDialog" title="Add Transaction" href="@Url.Action("Create", "Transactions", new { accountId = ViewBag.Account.AccountId })">
+//                                    <img alt="Add Transaction" title="Add Transaction" class="imgIcons" style="text-align: right;" src="~/Images/add.png" />
+//                                </a>
+//                            }
+//                    
+                    ?>
+                </td>
+            </tr>
+        </tbody>
+        <tfoot></tfoot>
+    </table>
+}
+</div>
+<div>
+    <table>
+        <thead></thead>
+        <tbody>
+            <?php
+//                @foreach (EnvelopesApplication.Models.vwUserTransaction ut in (List<EnvelopesApplication.Models.vwUserTransaction>)ViewBag.PendingGroup)
+//                {
+//                    <tr>
+//                        <td>@ut.TransactionName</td>
+//                        <td>@ut.TransactionPending</td>
+//                        <td>
+//                            <a href="@Url.Action("MoveNamedPending", "Transactions", new { accountId = ut.AccountId, transactionName = ut.TransactionName })">
+//                                <img alt="Move Pending for Group" title="Move Pending for Grop" class="imgIcons" style="text-align: right;" src="~/Images/movePending.jpeg" /></a>
+//                        </td>
+//                    </tr>
+//                }
+            ?>
+        </tbody>
+        <tfoot></tfoot>
+    </table>
+</div>
+<div class="editControlSwitch" style="float: right; margin-left: 40px;">
     <img alt="Add/Edit/Delete Accounts" title="Add/Edit/Delete Envelopes" class="imgIcons" style="text-align: right;" src="Images/pencil.png" />
 </div>
-<div style="float: right; margin-left: 40px; margin-right: 40px;">
-    <a class="showDialog" title="Expenses Graph" href="@Url.Action(\"_Chart\", \"Accounts\")">
+<div style="float: right; margin-left: 40px;">
+    <a href="@Url.Action("AddPaycheck", "Transactions", new { accountId = ViewBag.Account.AccountId })">
+       <img alt="Add a Paycheck" title="Add a Paycheck" class="imgIcons" style="text-align: right;" src="Images/paycheck.jpg" /></a>
+</div>
+<div style="float: right; margin-left: 40px;">
+    <a class="showDialog" title="Transfer Funds" href="@Url.Action("AddTransfer", "Transactions", new { accountId = ViewBag.Account.AccountId })">
+       <img alt="Transfer Funds" title="Transfer Funds" class="imgIcons" style="text-align: right;" src="Images/transfer.png" /></a>
+</div>
+<div style="float: right; margin-left: 40px;">
+    <a class="showDialog" title="Expenses Graph" href="@Url.Action("_Chart", "Envelopes", new { accountId = ViewBag.Account.AccountId })">
        <img alt="Expense Graph" title="Expense Graphs" class="imgIcons" style="text-align: right;" src="Images/pie.png" /></a>
 </div>
+<h2 class="">Account Name in COLOR</h2>
+
+
 <div class="clear"></div>
 <p class="editControls hiddenEditControls">
     <?= Html::a('Create Envelope', ['create', 'accountId' => $account->Id], ['class' => 'btn btn-success showDialog', 'title' => 'Create Envelope']) ?>
@@ -37,35 +110,47 @@ $this->params['breadcrumbs'][] = $this->title;
         </th>
         <th class="editControls">Pending
         </th>
+        <th class="editControls">Cost/Month
+        </th>
+        <th class="editControls">Remaining
+        </th>
     </tr>
-    
+
     <?php foreach ($envelopes as $envelope): ?>
         <tr>
             <td class="editControls hiddenEditControls">                
-    <?= Html::a('Edit', ['update', 'id' => $envelope->Id], ['class' => 'showDialog', 'title' => 'Edit Envelope']) ?>
-    <?= Html::a('Details', ['view', 'id' => $envelope->Id], ['class' => 'showDialog', 'title' => 'View Envelope']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $envelope->Id], [
-            'class' => 'linkConfirm',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>                
+                <?= Html::a('Edit', ['update', 'id' => $envelope->Id], ['class' => 'showDialog', 'title' => 'Edit Envelope']) ?>
+                <?= Html::a('Details', ['view', 'id' => $envelope->Id], ['class' => 'showDialog', 'title' => 'View Envelope']) ?>
+                <?=
+                Html::a('Delete', ['delete', 'id' => $envelope->Id], [
+                    'class' => 'linkConfirm',
+                    'data' => [
+                        'confirm' => 'Are you sure you want to delete this item?',
+                        'method' => 'post',
+                    ],
+                ])
+                ?>                
             </td>
             <td class="<?= $envelope->Color ?>">
-                <?= Html::a($envelope->Name, ['transaction/index', 'envelopeId' => $envelope->Id], ['class' => $envelope->Color, 'title' => $envelope->Name]) ?>
+    <?= Html::a($envelope->Name, ['transaction/index', 'envelopeId' => $envelope->Id], ['class' => $envelope->Color, 'title' => $envelope->Name]) ?>
             </td>
             <td class="editControls">
-                0.00<?php // ($account->vwAccountSum !== null ? $account->vwAccountSum->AccountSum : '0.00') ?>
+                0.00<?php // ($account->vwAccountSum !== null ? $account->vwAccountSum->AccountSum : '0.00')  ?>
             </td>
             <td class="editControls">
-                0.11<?php // ($account->vwAccountSum !== null ? $account->vwAccountSum->AccountPending : '0.00') ?>
+                0.11<?php // ($account->vwAccountSum !== null ? $account->vwAccountSum->AccountPending : '0.00')  ?>
+            </td>
+            <td class="editControls">
+                0.22<?php // ($account->vwAccountSum !== null ? $account->vwAccountSum->AccountPending : '0.00')  ?>
+            </td>
+            <td class="editControls">
+                0.33<?php // ($account->vwAccountSum !== null ? $account->vwAccountSum->AccountPending : '0.00')  ?>
             </td>
         </tr>
-    <?php endforeach; ?>
+<?php endforeach; ?>
 </table>
 <?php $this->endBlock(); ?>
 
 <?php $this->beginBlock('secondary-one'); ?>
-    list of transactions
+list of transactions
 <?php $this->endBlock(); ?>
