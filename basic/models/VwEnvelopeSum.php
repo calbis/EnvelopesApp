5bc33bf0d1c2db51b5c85d4632ff2a87892ad951
calvinb <calvinb@calvinb>
2014-12-04 20:42:05 -0500

    generated model for vw_envelope_sum

---
+++ basic/models/VwEnvelopeSum.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "vw_envelope_sum".
+ *
+ * @property integer $EnvelopeId
+ * @property string $EnvelopeSum
+ * @property string $EnvelopePending
+ * @property string $StatsCost
+ * @property string $TimeLeft
+ * @property string $GoalDeposit
+ */
+class VwEnvelopeSum extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'vw_envelope_sum';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['EnvelopeId'], 'integer'],
+            [['EnvelopeSum', 'EnvelopePending', 'StatsCost', 'TimeLeft', 'GoalDeposit'], 'number']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'EnvelopeId' => 'Envelope ID',
+            'EnvelopeSum' => 'Envelope Sum',
+            'EnvelopePending' => 'Envelope Pending',
+            'StatsCost' => 'Stats Cost',
+            'TimeLeft' => 'Time Left',
+            'GoalDeposit' => 'Goal Deposit',
+        ];
+    }
+}
